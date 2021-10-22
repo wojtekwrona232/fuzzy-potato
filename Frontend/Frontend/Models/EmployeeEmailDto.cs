aeb6714bdd3f7914f58883b575b84444b7eb703d
Quadium9 <piotrek.stec1@gmail.com>
2021-10-22 19:25:51 +0200

    Revert "changes"
    This reverts commit 459232bf7be0cd2bec5248f1f6febea3c4a4e99d.

---
+++ Frontend/Frontend/Models/EmployeeEmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Dtos
+{
+    public class EmployeeEmailDto
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Frontend/Frontend/Models/EmployeesModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Models
+{
+    public class EmployeesModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string Link { get; set; }
+    }
+}
