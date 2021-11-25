061ffff4e8848e5c6ad784e37b4eea86d4179444
Wojtek <unity-99@hotmail.com>
2021-11-25 17:08:45 +0100

    last push

--- Frontend/Frontend/Program.cs
+++ Frontend/Frontend/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,11 +14,33 @@ namespace Frontend
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            
+            // use this to allow command line parameters in the config
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+
+            var hostUrl = configuration["applicationUrl"];
+            if (string.IsNullOrEmpty(hostUrl))
+                hostUrl = "https://*:5101;https://0.0.0.0:5101";
+
+
+            var host = new WebHostBuilder()
+                .UseKestrel()                
+                .UseUrls(hostUrl)   // <!-- this 
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseConfiguration(configuration)
+                .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseKestrel().UseStartup<Startup>(); });
     }
 }
